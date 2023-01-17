fe5381cd7b9648331377a42b38def9cad394ca94
benNjogu <kamandemachafi@gmail.com>
2023-01-17 10:23:25 +0300

    Getting the server up and running

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors);
+
+const CONNECTION_URL =
+  "mongodb+srv://KeyMan:KeyMan123@cluster0.n5iep0z.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((e) => console.log(e.message));
