684acb2c8891f4a90c402187624b93f4959723ca
benNjogu <kamandemachafi@gmail.com>
2023-01-22 14:54:20 +0300

    Refactoring and cleaning

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb://localhost/memories";
-  // "mongodb+srv://KeyMan:KeyMan123@cluster0.n5iep0z.mongodb.net/?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
